29c38b49f0de4416eff59c831750a8be78631c03
Marcin Kochanek <md.kochanek@gmail.com>
2017-02-20 23:11:21 +0100

    Add new grunt file

---
+++ Gruntfile.js
@@ -0,0 +1,60 @@
+module.exports = function(grunt) {
+
+  	// Project configuration.
+	grunt.initConfig({
+	  	sass: {
+	  		options: {
+	  			sourceMap: true
+	  		},
+	  		dist: {
+	  			files: {
+	  				'style.css': 'style.sass'
+	  			}
+	  		}
+	  	},
+
+	  	imagemin: {
+	  		dynamic: {
+	  			files: [{
+	  				expand: true,
+	  				cwd: 'images/',
+	  				src: ['**/*.{png,jpg,gif}'],
+	  				dest: 'images/build/'
+	  			}]
+	  		}
+	  	}
+
+	  	watch: {
+	    	scripts: {
+		        files: ['sass/*.sass'],
+		        tasks: ['sass'],
+		        options: {
+		            spawn: false,
+		        },
+	    	} 
+		}
+
+		browserSync: {
+	        dev: {
+	            files: {
+	                src: [
+	                    'app/css/*.css',
+	                        'app/*.html'
+	                    ]
+	                },
+	                options: {
+	                    watchTask: true,
+	                    server: './app'
+	                }
+	            }
+	        }
+  	});
+  	
+  	// Load the plugins tasks 
+  	grunt.loadNpmTasks('grunt-sass');
+ 	grunt.loadNpmTasks('grunt-contrib-imagemin');
+	grunt.loadNpmTasks('grunt-browser-sync');
+ 	// Default task(s).
+  
+  	grunt.registerTask('default', ['sass', 'imagemin']);
+};
